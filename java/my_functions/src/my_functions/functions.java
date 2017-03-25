a614c322132826e03a1e266d15fc774f5adb60f4
hugh712 <hugh712@gmail.com>
2017-03-25 15:38:09 +0800

    add MissingInteger

--- java/my_functions/src/my_functions/functions.java
+++ java/my_functions/src/my_functions/functions.java
@@ -217,8 +217,37 @@ public class functions {
 		}
 		return -1;
 		
+	}
+	
+	//Find the minimal positive integer not occurring in a given sequence.
+	static public int MissingInteger(int[] A)
+	{
+		 int max = A.length;
+		    int threshold = 1;
+		    boolean[] bitmap = new boolean[max + 1];
+
+		    //populate bitmap and also find highest positive int in input list.
+		    for (int i = 0; i < A.length; i++) {
+		        if (A[i] > 0 && A[i] <= max) {
+		            bitmap[A[i]] = true;
+		        }
+
+		        if (A[i] > threshold) {
+		            threshold = A[i];
+		        }
+		    }
+
+		    //find the first positive number in bitmap that is false.
+		    for (int i = 1; i < bitmap.length; i++) {
+		        if (!bitmap[i]) {
+		            return i;
+		        }
+		    }
+
+		    //this is to handle the case when input array is not missing any element.
+		    return (threshold+1);
+		
 		
 		
 	}
-	
 }
