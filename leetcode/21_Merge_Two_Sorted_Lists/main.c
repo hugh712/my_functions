b59fdfabe1b443c79323c3afa7eac749b642344e
hugh712 <hugh712@gmail.com>
2017-12-29 08:59:48 +0800

    update frame of 21_Merge_Two_Sorted_Lists

---
+++ leetcode/21_Merge_Two_Sorted_Lists/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+struct ListNode {
+     int val;
+     struct ListNode *next;
+};
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2);
+
+int main(void)
+{
+
+
+	return 0;
+}
+
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
+    
+}
