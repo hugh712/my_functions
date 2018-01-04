5f08c2272b02a24157d0b6d2b6051d3e2e258216
hugh712 <hugh712@gmail.com>
2018-01-04 11:22:05 +0800

    update 28_Implement_strStr/main.c

--- leetcode/28_Implement_strStr/main.c
+++ leetcode/28_Implement_strStr/main.c
@@ -4,7 +4,7 @@
 int strStr(char* haystack, char* needle);
 int main(void)
 {
-	char * haystack="hello";
+	char * haystack="ppsshello";
 	char * needle="ll";
 	int output=0;
 
@@ -14,10 +14,37 @@ int main(void)
 }
 int strStr(char* haystack, char* needle)
 {
-	int len=0;	
+	int lenHaystack=0;	
+	int lenNeedle=0;	
+	int point_e=0;
+	int match=0;
 
-	printf("strlen=%ld\n", strlen(haystack));
-	printf("strlen=%ld\n", strlen(needle));
+	lenHaystack=strlen(haystack);
+	lenNeedle=strlen(needle);
 
-	return len;
+	if((lenNeedle==0 && lenHaystack==0) || lenNeedle==0)
+		return 0;
+	else if(lenHaystack==0 && lenNeedle!=0)
+		return -1;
+
+	for (int i=0;i<lenHaystack;i++)
+	{
+		if(haystack[i]==needle[0])
+		{
+			match=0;
+			for(int j=0;j<lenNeedle;j++)
+			{
+				if(haystack[i+j]==needle[j])
+				{
+					match++;
+					if(match==lenNeedle)
+					{
+						return i;
+					}
+				}
+			}//end of inner for
+		}//end of if	
+	}//end of for
+
+	return -1;
 }
